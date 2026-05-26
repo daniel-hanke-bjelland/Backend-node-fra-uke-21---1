46df0e4bf04969d3b90d7b843634326cfe878da6
Daniel Hanke Bjelland <danielhankebjelland@MacBook-Air.local>
2026-05-26 15:18:18 +0200

    Laget ferdig førsteutkast til å laste opp og sjekke og logge inn i mongoGB.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const argon2 = require("argon2");
 const mongodb = require("mongoose");
 
+const User = require("./models/User");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -20,11 +21,30 @@ app.get("/registrer", (req, res) => {
     res.render("registrer");
 })
 
-app.post("/", (req, res) => {
+app.get("/velkommen", (req, res) => {
+    // res.send("HELLO")
+    res.render("velkommen");
+})
+
+app.post("/", async (req, res) => {
     const {email, passord} = req.body;
     console.log(req.body);
 
-    res.send("OK");
+    const user = await User.findOne({email});
+    console.log(user);
+
+    if (!user) {
+        res.send("Bruker eksisterer ikke i databasen");
+    }
+
+    const isMatch = await argon2.verify(user.passord, passord);
+    console.log(isMatch);
+    
+    if (isMatch) {
+        res.redirect("/velkommen");
+    }
+
+    // res.send("OK");
 })
 
 app.post("/registrer", async (req, res) => {
@@ -36,7 +56,15 @@ app.post("/registrer", async (req, res) => {
 
         console.log(hashtPassord);
 
-        res.send(`email: ${email}, passord ${passord}, gjenta passord: ${gjentaPassord}, kryptert passord: ${hashtPassord}`);   
+        const user = new User({
+            email: email,
+            passord: hashtPassord
+        });
+
+        await user.save();
+
+        res.redirect("/");
+        // res.send(`email: ${email}, passord ${passord}, gjenta passord: ${gjentaPassord}, kryptert passord: ${hashtPassord}`);   
     } else {
         res.send("Passord og gjenta passord matcher ikke");
     }
